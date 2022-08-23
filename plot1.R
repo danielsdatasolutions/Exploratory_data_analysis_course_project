694a762829c178766dfc46331b8fd46c75550eea
Drew Daniels <79292238+danielsdatasolutions@users.noreply.github.com>
2022-08-23 13:52:58 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,8 +3,10 @@ data <- "Desktop/Datasets/household_power_consumption.txt"
 power_data <- read.table(data, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subset_data <- power_data[power_data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-# Creating plot1
+# Converting variable for use in plot
 global_active_power <- as.numeric(subset_data$Global_active_power)
+
+# Creating plot
 png("plot1.png", width=480, height=480)
 hist(global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
